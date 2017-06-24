1b4706727d3572a84f756b618d6de9dcf73194c8
msukkari <michael.sukkarieh@mail.mcgill.ca>
2017-06-23 22:15:14 -0700

    added basic level/tile structure

---
+++ Yves/Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    public List<Tile> m_TileList;
+
+	// Use this for initialization
+	void Start () {
+        LoadTileList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadTileList()
+    {
+        // Instantiate new TileList
+        m_TileList = new List<Tile>();
+
+        // Loop and add all tiles
+        foreach (Transform child in transform)
+        {
+            Debug.Log("IN FOR EACH");
+            m_TileList.Add(child.GetComponent<Tile>());
+        }
+    }
+}
